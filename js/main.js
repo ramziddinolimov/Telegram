b2ecdbdd88cc2b2112a9c8ac4dd1671648c7a1eb
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-05-18 15:45:48 +0500

    javascript main

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const hamburgerButtonElement = document.querySelector("#hamburgerButton")
+const menuElement = document.querySelector(".menu")
+const rightAppMain = document.querySelector(".right_app")
+const shadowELement = document.querySelector(".shadow")
